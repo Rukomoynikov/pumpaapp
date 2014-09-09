303beffa160fbc1c2e8cad43beaa10dbd6dc663b
Maksim Rukomoynikov <cat@mos-it.com>
2014-09-09 12:01:39 +0400

    Deleted ios platform, move to PhoneGap build

--- www/cordova_plugins.js
+++ www/cordova_plugins.js
@@ -20,13 +20,21 @@ module.exports = [
         "clobbers": [
             "device"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "clobbers": [
+            "window.open"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "org.apache.cordova.console": "0.2.9",
-    "org.apache.cordova.device": "0.2.10"
+    "org.apache.cordova.device": "0.2.10",
+    "org.apache.cordova.inappbrowser": "0.5.1"
 }
 // BOTTOM OF METADATA
 });
